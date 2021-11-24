5f0dca9c8d730498e2c3955a2b67c5fabe79f581
SHHristov20 <SHHristov20@codingburgas.bg>
2021-11-24 10:36:18 +0200

    Create calculators menu

---
+++ applicaton/PhysicsProject/calculatorsMenu.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+void calculators()
+{
+	system("cls");
+	std::cout << std::endl << "\t\t\t\t   ____      _            _       _                 " << std::endl;
+	std::cout << "\t\t\t\t  / ___|__ _| | ___ _   _| | __ _| |_ ___  _ __ ___ " << std::endl;
+	std::cout << "\t\t\t\t | |   / _` | |/ __| | | | |/ _` | __/ _ \\| '__/ __|" << std::endl;
+	std::cout << "\t\t\t\t | |__| (_| | | (__| |_| | | (_| | || (_) | |  \\__ \\" << std::endl;
+	std::cout << "\t\t\t\t  \\____\\__,_|_|\\___|\\__,_|_|\\__,_|\\__\\___/|_|  |___/" << std::endl << std::endl << std::endl << std::endl;
+	std::cout << "Choose:" << std::endl;
+	std::cout << "1.Increasing acceleration" << std::endl;
+	std::cout << "2.Decreasing acceleration" << std::endl;
+	std::cout << "3.Free fall" << std::endl;
+	std::cout << "4.Mechanical work and power" << std::endl;
+	std::cout << "5.Hydrostatic pressure" << std::endl;
+	std::cout << "6.Pressure" << std::endl;
+	std::cout << "7.Arhimed's law" << std::endl;
+	std::cout << "8.Convert units of measurement" << std::endl;
+	std::cout << "9.Formulas" << std::endl;
+	std::cout << "10.Back" << std::endl;
+}
