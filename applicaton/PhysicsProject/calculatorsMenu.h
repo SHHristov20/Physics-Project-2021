ff5cae4dc11bd4d15eb28798a6f5ec07e3fcba7c
Maxim <MMMarinov20@codingburgas.bg>
2021-11-24 12:33:15 +0200

    Create dynamic calculators menu

--- applicaton/PhysicsProject/calculatorsMenu.h
+++ applicaton/PhysicsProject/calculatorsMenu.h
@@ -1,22 +1,107 @@
 #pragma once
+#pragma once
 #include <iostream>
-void calculators()
-{
-	system("cls");
+
+void printTitle() {
 	std::cout << std::endl << "\t\t\t\t   ____      _            _       _                 " << std::endl;
 	std::cout << "\t\t\t\t  / ___|__ _| | ___ _   _| | __ _| |_ ___  _ __ ___ " << std::endl;
 	std::cout << "\t\t\t\t | |   / _` | |/ __| | | | |/ _` | __/ _ \\| '__/ __|" << std::endl;
 	std::cout << "\t\t\t\t | |__| (_| | | (__| |_| | | (_| | || (_) | |  \\__ \\" << std::endl;
 	std::cout << "\t\t\t\t  \\____\\__,_|_|\\___|\\__,_|_|\\__,_|\\__\\___/|_|  |___/" << std::endl << std::endl << std::endl << std::endl;
-	std::cout << "Choose:" << std::endl;
-	std::cout << "1.Increasing acceleration" << std::endl;
-	std::cout << "2.Decreasing acceleration" << std::endl;
-	std::cout << "3.Free fall" << std::endl;
-	std::cout << "4.Mechanical work and power" << std::endl;
-	std::cout << "5.Hydrostatic pressure" << std::endl;
-	std::cout << "6.Pressure" << std::endl;
-	std::cout << "7.Arhimed's law" << std::endl;
-	std::cout << "8.Convert units of measurement" << std::endl;
-	std::cout << "9.Formulas" << std::endl;
-	std::cout << "10.Back" << std::endl;
+}
+
+
+void activeChoise(bool one, bool two, bool three, bool four, bool five, bool six, bool seven, bool eight, bool nine, bool ten)
+{
+	printTitle();
+	if (one)
+	{
+		std::cout << "-> " << "Increasing acceleration" << std::endl;
+	}
+	else
+	{
+		std::cout << "   Increasing acceleraition" << std::endl;
+	}
+
+	if (two)
+	{
+		std::cout << "-> " << "Decreasing acceleration" << std::endl;
+	}
+	else
+	{
+		std::cout << "   Decreasing acceleration" << std::endl;
+	}
+
+	if (three)
+	{
+		std::cout << "-> " << "Free fall" << std::endl;
+	}
+	else
+	{
+		std::cout << "   Free fall" << std::endl;
+	}
+
+	if (four)
+	{
+		std::cout << "-> " << "Mechanical work and power" << std::endl;
+	}
+	else
+	{
+		std::cout << "   Mechanical work and power" << std::endl;
+	}
+
+	if (five)
+	{
+		std::cout << "-> " << "Hydrostatic pressure" << std::endl;
+	}
+	else
+	{
+		std::cout << "   Hydrostatic pressure" << std::endl;
+	}
+
+	if (six)
+	{
+		std::cout << "-> " << "Pressure" << std::endl;
+	}
+	else
+	{
+		std::cout << "   Pressure" << std::endl;
+	}
+
+	if (seven)
+	{
+		std::cout << "-> " << "Arhimed's law" << std::endl;
+	}
+	else
+	{
+		std::cout << "   Arhimed's law" << std::endl;
+	}
+	if (eight)
+	{
+		std::cout << "-> " << "Convert units of measurement" << std::endl;
+	}
+	else
+	{
+		std::cout << "   Convert units of measurement" << std::endl;
+	}
+	if (nine)
+	{
+		std::cout << "-> " << "Formulas" << std::endl;
+	}
+	else
+	{
+		std::cout << "   Formulas" << std::endl;
+	}
+
+	if (ten)
+	{
+		std::cout << "-> " << "Back" << std::endl;
+	}
+	else
+	{
+		std::cout << "   Back" << std::endl;
+	}
+}
+void printCalculatorsMenu() {
+	activeChoise(true, false, false, false, false, false, false, false, false, false);
 }
