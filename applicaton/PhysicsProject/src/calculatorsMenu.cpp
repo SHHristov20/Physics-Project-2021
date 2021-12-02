f2bc4940781e8616ccfaa2f71f75f51d99ebe17b
Maxim <MMMarinov20@codingburgas.bg>
2021-12-02 17:43:56 +0200

    Center calculators menu

--- applicaton/PhysicsProject/src/calculatorsMenu.cpp
+++ applicaton/PhysicsProject/src/calculatorsMenu.cpp
@@ -12,47 +12,47 @@
 #include "ArhimedsLaw.h"
 #include "convertUnits.h"
 void printTitle() {
-	std::cout << std::endl << "\t\t\t\t   ____      _            _       _                 " << std::endl;
-	std::cout << "\t\t\t\t  / ___|__ _| | ___ _   _| | __ _| |_ ___  _ __ ___ " << std::endl;
-	std::cout << "\t\t\t\t | |   / _` | |/ __| | | | |/ _` | __/ _ \\| '__/ __|" << std::endl;
-	std::cout << "\t\t\t\t | |__| (_| | | (__| |_| | | (_| | || (_) | |  \\__ \\" << std::endl;
-	std::cout << "\t\t\t\t  \\____\\__,_|_|\\___|\\__,_|_|\\__,_|\\__\\___/|_|  |___/" << std::endl << std::endl << std::endl << std::endl;
+	std::cout << std::endl << "\t\t\t\t\t\t\t\t\t\t\t   ____      _            _       _                 " << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  / ___|__ _| | ___ _   _| | __ _| |_ ___  _ __ ___ " << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t | |   / _` | |/ __| | | | |/ _` | __/ _ \\| '__/ __|" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t | |__| (_| | | (__| |_| | | (_| | || (_) | |  \\__ \\" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  \\____\\__,_|_|\\___|\\__,_|_|\\__,_|\\__\\___/|_|  |___/" << std::endl << std::endl << std::endl << std::endl;
 }
 
 void design() {
-	std::cout << " _______________________________________________" << std::endl;
-	std::cout << "|  ___________________________________________  |" << std::endl;
-	std::cout << "| |  Welcome to our Physics calculator!       | |" << std::endl;
-	std::cout << "| |  Choose yours and press ENTER!            | |" << std::endl;
-	std::cout << "| |                                           | |" << std::endl;
-	std::cout << "| |  (Navigate using W, A, S, D)              | |" << std::endl;
-	std::cout << "| |___________________________________________| |" << std::endl;
-	std::cout << "|  _________________         _________________  |" << std::endl;
-	std::cout << "| |    Increasing   |       |   Hydrostatic   | |" << std::endl;
-	std::cout << "| |   Acceleration  |       |    Pressure     | |" << std::endl;
-	std::cout << "| |                 |       |                 | |" << std::endl;
-	std::cout << "| |_________________|       |_________________| |" << std::endl;
-	std::cout << "|  _________________         _________________  |" << std::endl;
-	std::cout << "| |    Decreasing   |       |     Pressure    | |" << std::endl;
-	std::cout << "| |   Acceleration  |       |                 | |" << std::endl;
-	std::cout << "| |                 |       |                 | |" << std::endl;
-	std::cout << "| |_________________|       |_________________| |" << std::endl;
-	std::cout << "|  _________________         _________________  |" << std::endl;
-	std::cout << "| |	 Free       |       |     Arhimed's   | |" << std::endl;
-	std::cout << "| |      Fall       |       |       law       | |" << std::endl;
-	std::cout << "| |                 |       |                 | |" << std::endl;
-	std::cout << "| |_________________|       |_________________| |" << std::endl;
-	std::cout << "|  _________________         _________________  |" << std::endl;
-	std::cout << "| |      Work       |       |     Convert     | |" << std::endl;
-	std::cout << "| |      Power      |       |      Units      | |" << std::endl;
-	std::cout << "| |                 |       |                 | |" << std::endl;
-	std::cout << "| |_________________|       |_________________| |" << std::endl;
-	std::cout << "|  _________________        __________________  |" << std::endl;
-	std::cout << "| |    Formulas     |       |       Back      | |" << std::endl;
-	std::cout << "| |                 |       |                 | |" << std::endl;
-	std::cout << "| |                 |       |                 | |" << std::endl;
-	std::cout << "| |_________________|       |_________________| |" << std::endl;
-	std::cout << "|_______________________________________________|" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t   _______________________________________________" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  |  ___________________________________________  |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |  Welcome to our Physics calculator!       | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |  Choose yours and press ENTER!            | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |                                           | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |  (Navigate using W, A, S, D)              | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |___________________________________________| |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  |  _________________         _________________  |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |    Increasing   |       |   Hydrostatic   | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |   Acceleration  |       |    Pressure     | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |                 |       |                 | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |_________________|       |_________________| |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  |  _________________         _________________  |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |    Decreasing   |       |     Pressure    | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |   Acceleration  |       |                 | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |                 |       |                 | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |_________________|       |_________________| |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  |  _________________         _________________  |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |	   Free       |       |     Arhimed's   | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |      Fall       |       |       law       | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |                 |       |                 | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |_________________|       |_________________| |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  |  _________________         _________________  |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |      Work       |       |     Convert     | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |      Power      |       |      Units      | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |                 |       |                 | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |_________________|       |_________________| |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  |  _________________        __________________  |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |    Formulas     |       |       Back      | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |                 |       |                 | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |                 |       |                 | |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  | |_________________|       |_________________| |" << std::endl;
+	std::cout << "\t\t\t\t\t\t\t\t\t\t\t  |_______________________________________________|" << std::endl;
 }
 
 void enteredChoice() {
@@ -97,60 +97,60 @@ void activeChoise(bool one, bool two, bool three, bool four, bool five, bool six
 	if (one)
 	{
 		design();
-		gotoXY(9, 19);
+		gotoXY(99, 19);
 		std::cout << "±±±±±";
 	}
 	if (two)
 	{
 		design();
-		gotoXY(9, 24);
+		gotoXY(99, 24);
 		std::cout << "±±±±±";
 	}
 	if (three)
 	{
 		design();
-		gotoXY(9, 29);
+		gotoXY(99, 29);
 		std::cout << "±±±±±";
 	}
 	if (four)
 	{
 		design();
-		gotoXY(9, 34);
+		gotoXY(99, 34);
 		std::cout << "±±±±±";
 	}
 	if (five)
 	{
 		design();
-		gotoXY(9, 39);
+		gotoXY(99, 39);
 		std::cout << "±±±±±";
 	}
 	if (six)
 	{
 		design();
-		gotoXY(35, 19);
+		gotoXY(125, 19);
 		std::cout << "±±±±±";
 	}
 	if (seven)
 	{
 		design();
-		gotoXY(35, 24);
+		gotoXY(125, 24);
 		std::cout << "±±±±±";
 	}
 	if (eight)
 	{
 		design();
-		gotoXY(35, 29);
+		gotoXY(125, 29);
 		std::cout << "±±±±±";
 	}
 	if (nine)
 	{
 		design();
-		gotoXY(35, 34);
+		gotoXY(125, 34);
 		std::cout << "±±±±±";
 	}
 	if (ten) {
 		design();
-		gotoXY(35, 39);
+		gotoXY(125, 39);
 		std::cout << "±±±±±";
 	}
 
