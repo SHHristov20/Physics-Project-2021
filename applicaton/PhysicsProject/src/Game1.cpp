f317d72568e1450c4e7dd163e28d08ef7f970728
MMPetrov20 <MMPetrov20@codingburgas.bg>
2021-12-03 21:24:35 +0200

    Create game textures for "Catch the leaf"

--- applicaton/PhysicsProject/src/Game1.cpp
+++ applicaton/PhysicsProject/src/Game1.cpp
@@ -8,8 +8,20 @@
 void startGame1() 
 {
 	sf::RenderWindow window(sf::VideoMode(1500, 1000), "Catch the leaf", sf::Style::Default);
-
 	window.setFramerateLimit(60);
+
+	sf::RectangleShape object(sf::Vector2f(100, 100));
+	object.setOrigin(-700.0f, -450.0f);
+
+	sf::Texture objectTexture;
+	objectTexture.loadFromFile("../PhysicsProject/images/leaf.png");
+	object.setTexture(&objectTexture);
+
+	sf::Texture background;
+	background.loadFromFile("../PhysicsProject/images/grass-background.jpg");
+	sf::Sprite Background(background);
+
+	
 
 	while (window.isOpen())
 	{
@@ -23,5 +35,10 @@ void startGame1()
 				start();
 			}
 		}
+
+		window.clear();
+		window.draw(Background);
+		window.draw(object);
+		window.display();
 	}
 }
