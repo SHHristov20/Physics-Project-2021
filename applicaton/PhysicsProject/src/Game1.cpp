ad30d9a75dae73cc189de865a4e7bf61ddb7b572
MMPetrov20 <MMPetrov20@codingburgas.bg>
2021-12-03 22:49:43 +0200

    Create leaf movement

--- applicaton/PhysicsProject/src/Game1.cpp
+++ applicaton/PhysicsProject/src/Game1.cpp
@@ -5,6 +5,13 @@
 #include "Game1.h"
 #include "front-end.h"
 
+int assignDirection(int index, int Array[])
+{
+	srand(time(NULL));
+	Array[index + 1] = rand() % 8;
+	return Array[index + 1];
+}
+
 void startGame1() 
 {
 	sf::RenderWindow window(sf::VideoMode(1500, 1000), "Catch the leaf", sf::Style::Default);
@@ -23,22 +30,67 @@ void startGame1()
 
 	
 
-	while (window.isOpen())
-	{
-		sf::Event evnt;
+    while (window.isOpen())
+    {
+        int direction[1000] = { 0 };
+        double speed = 0.03f;
 
-		while (window.pollEvent(evnt))
-		{
-			if (evnt.type == sf::Event::Closed)
-			{
-				window.close();
-				start();
-			}
-		}
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
 
-		window.clear();
-		window.draw(Background);
-		window.draw(object);
-		window.display();
-	}
+        for (int i = 0; i < 300; i++)
+        {
+            if (direction[i] == 0)
+            {
+                object.move(0.0f, speed);
+                assignDirection(i, direction);
+            }
+            else if (direction[i] == 1)
+            {
+                object.move(speed, speed);
+                assignDirection(i, direction);
+            }
+            else if (direction[i] == 2)
+            {
+                object.move(speed, 0.0f);
+                assignDirection(i, direction);
+            }
+            else if (direction[i] == 3)
+            {
+                object.move(speed, -speed);
+                assignDirection(i, direction);
+            }
+            else if (direction[i] == 4)
+            {
+                object.move(0.0f, -speed);
+                assignDirection(i, direction);
+            }
+            else if (direction[i] == 5)
+            {
+                object.move(-speed, -speed);
+                assignDirection(i, direction);
+            }
+            else if (direction[i] == 6)
+            {
+                object.move(-speed, 0.0f);
+                assignDirection(i, direction);
+            }
+            else if (direction[i] == 7)
+            {
+                object.move(-speed, speed);
+                assignDirection(i, direction);
+            }
+        }
+
+        window.clear();
+        window.draw(Background);
+        window.draw(object);
+        window.display();
+    }
 }
