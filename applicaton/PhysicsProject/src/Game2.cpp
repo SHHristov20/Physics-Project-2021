e80ff93c476cb5f87be25ac414e6e4dfa70c30e8
Maxim <MMMarinov20@codingburgas.bg>
2021-12-04 15:41:30 +0200

    Implement character movment

--- applicaton/PhysicsProject/src/Game2.cpp
+++ applicaton/PhysicsProject/src/Game2.cpp
@@ -3,21 +3,21 @@
 #include <sfml\Graphics.hpp>
 #include "front-end.h"
 
-struct coords {
+struct coords { //platform coordinates
 	int x;
 	int y;
 };
 
 void startGame2() {
-	sf::RenderWindow window(sf::VideoMode(400, 533), "Doodle Jump", sf::Style::Default);
+	sf::RenderWindow window(sf::VideoMode(400, 533), "Doodle Jump", sf::Style::Default); // window creation
 	
-	window.setFramerateLimit(60);
+	window.setFramerateLimit(60); // FPS
 
-	sf::Texture background;
+	sf::Texture background; // Creating textures
 	sf::Texture platform;
 	sf::Texture character;
 
-	background.loadFromFile("../PhysicsProject/images/background.png");
+	background.loadFromFile("../PhysicsProject/images/background.png"); // Loading Textures
 	platform.loadFromFile("../PhysicsProject/images/platform.png");
 	character.loadFromFile("../PhysicsProject/images/character.png");
 
@@ -27,12 +27,17 @@ void startGame2() {
 
 	coords platf[20];
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 10; i++) // Making rndoom platform generation
 	{
 		platf[i].x = rand() % 400;
 		platf[i].y = rand() % 533;
 	}
 
+	int x = 100; // character coordinates
+	int y = 100;
+	int h = 200;
+	double dx = 0;
+	double dy = 0;
 	while (window.isOpen())
 	{
 		sf::Event evnt;
@@ -41,17 +46,35 @@ void startGame2() {
 		{
 			if (evnt.type == sf::Event::Closed)
 			{
-				window.close();
+				window.close(); // get back to the loading screen when the window is closed
 				start();
 			}
 		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) // Getting input for the movment
+		{
+			x = x + 3;
+		}
 
-		window.draw(Background);
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			x = x - 3;
+		}
+		dy = dy + 0.2;
+		y = y + dy; // character movment
+
+		if (y > 500) // if the character touches the border, bounce back
+		{
+			dy = dy - 10;
+		}
+
+		Character.setPosition(x, y);
+
+		window.draw(Background); // Drawing the textures
 		window.draw(Character);
 
 		for (int i = 0; i < 10; i++)
 		{
-			Platform.setPosition(platf[i].x, platf[i].y);
+			Platform.setPosition(platf[i].x, platf[i].y); // Setting the platform position
 			window.draw(Platform);
 		}
 
